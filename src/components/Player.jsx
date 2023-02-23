551c59a2ec9f1b2ff45ff488283ad0a24375ecc0
Anna Kovalik <annhuddy13@gmail.com>
2023-02-23 15:38:45 +0300

    Feat: implement basic player logic

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,14 +1,55 @@
-import { useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import style from "./Player.module.scss";
 
 function Player() {
     const [filled, setFilled] = useState(0);
-    const changeHandler = (e) => {
-        setFilled(e.target.value)
+    const [duration, setDuration] = useState(0);
+    const [audioInfo, setAudioInfo] = useState(0);
+    const [isPlaying, setIsPlaying] = useState(true);
+    const [elapsed, setElapsed] = useState(0)
+    const audioRef = useRef(null);
+    const intervalRef = useRef(null);
+
+    const changeTiming = (e) => {
+        if (audioRef.current) {
+            audioRef.current.currentTime = e.target.value
+        }
     }
+
+    useEffect(() => {
+        if (isPlaying) {
+            intervalRef.current = setInterval(() => {
+                const _elapsed = Math.floor(audioRef?.current?.currentTime);
+                setElapsed(_elapsed);
+            }, 100);
+        }
+
+        return () => {
+            clearInterval(intervalRef.current)
+        }
+    }, [isPlaying])
+
+    useEffect(() => {
+        async function getSong() {
+            const { data } = await fetch("https://saavn.me/songs?link=https://www.jiosaavn.com/song/thunderclouds/RT8zcBh9eUc").then(res => res.json())
+            setAudioInfo(data[0].downloadUrl[0].link)
+        }
+        getSong()
+    }, [])
     return (
         <div className={style.box}>
-            <input type="range" className={style.range} value={filled} onChange={e => changeHandler(e)} style={{ backgroundSize: `${filled * 100}%` }} max={1} step={0.01} />
+            <audio ref={audioRef} src={audioInfo} onLoadedData={(e) => {
+                setDuration(e.currentTarget.duration.toFixed(2))
+            }} />
+            <button onClick={() => { audioRef.current.play() }}>play</button>
+            <input
+                type="range"
+                className={`${style.range} ${!elapsed ? style.empty : ""}`}
+                value={elapsed}
+                onChange={e => changeTiming(e)}
+                max={100}
+                step={1}
+                style={{ background: `linear-gradient(to right, #0000FF ${elapsed}%, #121212 ${elapsed}%)` }} />
         </div>
     );
 }
